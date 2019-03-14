76958f7855d83dff95bb9c2d452e6a6c8aea34d9
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-03-14 11:21:41 +0200

    Fix the temp dir generation for more systems.

--- selenium.sh
+++ selenium.sh
@@ -8,7 +8,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-TMP_DIR=`mktemp -d -p "/tmp"`
+TMP_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 
 function setup_selenium_components {
 	echo "Changing to directory ${DIR}."
